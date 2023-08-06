c1f98412dfd2249868b77e1d2cf8d94f40169761
Im Code <141347375+Thecode764@users.noreply.github.com>
2023-08-07 01:27:27 +0800

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,7 @@
+a = input("Please Enter First Number:")
+b = input("Please Enter Second Number:")
+print"mark+ ",a + b
+print"mark- ",a - b
+print"mark* ",a * b
+print"mark% ",a % b
+print"mark/ ",a / b
